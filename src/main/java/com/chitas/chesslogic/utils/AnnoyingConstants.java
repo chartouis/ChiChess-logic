367281dc97af5d5e49ab3f1df948551127834afe
Chitos <erna95er@mail.ru>
2025-04-25 22:37:46 +0500

    modify acost

--- src/main/java/com/chitas/chesslogic/utils/AnnoyingConstants.java
+++ src/main/java/com/chitas/chesslogic/utils/AnnoyingConstants.java
@@ -3,25 +3,11 @@ package com.chitas.chesslogic.utils;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
-import com.chitas.chesslogic.model.User;
-import com.chitas.chesslogic.repo.UsersRepo;
-
-
 @Component
 public class AnnoyingConstants {
-    private final UsersRepo usersRepo;
 
-    public AnnoyingConstants(UsersRepo usersRepo){
-        this.usersRepo = usersRepo;
-
-    }
-
-    public User getCurrentUser(){
-        return usersRepo.findByUsername(SecurityContextHolder.getContext().getAuthentication().getName()).orElseThrow();
-    }
-
-    public String getCurrentUsername(){
-        return getCurrentUser().getUsername();
+    public String getCurrentUserUsername(){
+        return SecurityContextHolder.getContext().getAuthentication().getName();
     }
 
 }
