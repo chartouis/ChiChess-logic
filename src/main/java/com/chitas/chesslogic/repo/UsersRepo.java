3533977e3e5b0fa6107ea3fb5c93a59e64def2ee
Chitos <erna95er@mail.ru>
2025-04-25 22:20:42 +0500

    User to Optional<User> in UserRepo

--- src/main/java/com/chitas/chesslogic/repo/UsersRepo.java
+++ src/main/java/com/chitas/chesslogic/repo/UsersRepo.java
@@ -1,5 +1,7 @@
 package com.chitas.chesslogic.repo;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,7 +9,7 @@ import com.chitas.chesslogic.model.User;
 
 @Repository
 public interface UsersRepo extends JpaRepository<User, Long> {
-    User findByUsername(String username);
+    Optional<User> findByUsername(String username);
     boolean existsByEmail(String email);
     boolean existsByUsername(String username);
 
