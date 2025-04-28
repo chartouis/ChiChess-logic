dd60495915b0a9f0ca37d096a97d7bfc0eaa7a6a
Chitos <erna95er@mail.ru>
2025-04-28 21:33:45 +0500

    update RoomState

--- src/main/java/com/chitas/chesslogic/model/RoomState.java
+++ src/main/java/com/chitas/chesslogic/model/RoomState.java
@@ -4,80 +4,68 @@ import lombok.Data;
 
 @Data
 public class RoomState {
-    // room id
     private String id;
-
-    // if the game is live
     private boolean isActive;
+    private String creator;
+    private String black;
+    private String white;
+    private String position; // FEN
+    private String history; // SAN moves
 
-    // id of the player who created the room
-    private String creatorId;
-
-    // id of the player who entered the room
-    private String visitorId;
-
-    // position in FEN format
-    private String position;
-    
-    // history of moves in SAN format
-    private String history;
-
-    // Private constructor to enforce the use of Builder
     private RoomState(Builder builder) {
         this.id = builder.id;
         this.isActive = builder.isActive;
-        this.creatorId = builder.creatorId;
-        this.visitorId = builder.visitorId;
+        this.creator = builder.creator;
+        this.black = builder.black;
+        this.white = builder.white;
         this.position = builder.position;
         this.history = builder.history;
     }
 
-    // Static Builder class to create RoomState instances
     public static class Builder {
         private String id;
         private boolean isActive;
-        private String creatorId;
-        private String visitorId;
+        private String creator;
+        private String black;
+        private String white;
         private String position;
         private String history;
 
-        // Set room ID
         public Builder id(String id) {
             this.id = id;
             return this;
         }
 
-        // Set game status (active/inactive)
         public Builder isActive(boolean isActive) {
             this.isActive = isActive;
             return this;
         }
 
-        // Set creator's player ID
-        public Builder creatorId(String creatorId) {
-            this.creatorId = creatorId;
+        public Builder creator(String creator) {
+            this.creator = creator;
             return this;
         }
 
-        // Set visitor's player ID
-        public Builder visitorId(String visitorId) {
-            this.visitorId = visitorId;
+        public Builder black(String black) {
+            this.black = black;
             return this;
         }
 
-        // Set the current position in FEN format
+        public Builder white(String white) {
+            this.white = white;
+            return this;
+        }
+
         public Builder position(String position) {
             this.position = position;
             return this;
         }
 
-        // Set the move history in SAN format
         public Builder history(String history) {
             this.history = history;
             return this;
         }
 
-        // Build and return the RoomState object
         public RoomState build() {
             return new RoomState(this);
         }
