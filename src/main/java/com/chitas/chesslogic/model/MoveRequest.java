8a92ec57e11314256351789ae0386aea9c1fd8ef
Chitos <erna95er@mail.ru>
2025-04-28 21:33:10 +0500

    add Model for Move Requests

---
+++ src/main/java/com/chitas/chesslogic/model/MoveRequest.java
@@ -0,0 +1,11 @@
+package com.chitas.chesslogic.model;
+
+import lombok.Data;
+
+@Data
+public class MoveRequest {
+    private String from;   // e.g., "e7"
+    private String to;     // e.g., "e8"
+    private String promotion; // optional: "q", "r", "b", "n"
+}
+
