4de2a2362030f3e1ac0b7e712d2ea0c702e8ca9e
Chitos <erna95er@mail.ru>
2025-05-31 02:12:05 +0500

    security context now works. creating a minimal userDetails by relying on jwt username

---
+++ src/main/java/com/chitas/chesslogic/config/CUserDetailsService.java
@@ -0,0 +1,58 @@
+package com.chitas.chesslogic.config;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
+public class CUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return new UserDetails() {
+            @Override
+            public Collection<? extends GrantedAuthority> getAuthorities() {
+                return Collections.singleton(new SimpleGrantedAuthority("USER"));
+            }
+
+            @Override
+            public String getPassword() {
+                log.error("Attempt to get a password, but its pointless in this service");
+                return null;
+            }
+
+            @Override
+            public String getUsername() {
+                return username;
+            }
+
+            @Override
+            public boolean isAccountNonExpired() {
+                return true;
+            }
+
+            @Override
+            public boolean isAccountNonLocked() {
+                return true;
+            }
+
+            @Override
+            public boolean isCredentialsNonExpired() {
+                return true;
+            }
+
+            @Override
+            public boolean isEnabled() {
+                return true;
+            }
+        };
+    }
+
+}
