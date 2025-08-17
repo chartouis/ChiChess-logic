ec6d138bcd17801accc07d68f45090568ecf331c
Chitos <erna95er@mail.ru>
2025-08-17 23:55:26 +0500

    Chore: MessageRouter class

---
+++ src/main/java/com/chitas/chesslogic/service/MessageRouter.java
@@ -0,0 +1,63 @@
+package com.chitas.chesslogic.service;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import com.chitas.chesslogic.model.MoveRequest;
+import com.chitas.chesslogic.model.MoveResponse;
+import com.chitas.chesslogic.model.RoomState;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class MessageRouter {
+
+    private final ChessService chessService;
+
+    public MessageRouter(ChessService chessService) {
+        this.chessService = chessService;
+    }
+
+    public void handleMove(WebSocketSession session, JsonNode payload, Map<String, Set<WebSocketSession>> rooms)
+            throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        MoveRequest move = objectMapper.treeToValue(payload, MoveRequest.class);
+
+        String gameId = extractGameId(session);
+        Set<WebSocketSession> roomSessions = rooms.getOrDefault(gameId, Set.of());
+
+        String username = (String) session.getAttributes().get("username");
+
+        if (move == null || gameId == null || username == null) {
+            return;
+        }
+
+        boolean valid = chessService.doMove(gameId, move.getFrom(), move.getTo(), move.getPromotion(), username);
+        RoomState state = chessService.getRoomState(gameId);
+
+        MoveResponse response = new MoveResponse(valid, state);
+
+        String json = objectMapper.writeValueAsString(response);
+        TextMessage responseMessage = new TextMessage(json);
+
+        for (WebSocketSession s : roomSessions) {
+            if (s.isOpen()) {
+                s.sendMessage(responseMessage);
+                ;
+            }
+        }
+    }
+
+    public static String extractGameId(WebSocketSession session) {
+        // Example path: /ws/game/abc123
+        String path = Objects.requireNonNull(session.getUri()).getPath();
+        return path.substring(path.lastIndexOf("/") + 1);
+    }
+
+}
